1539beafe0b3fab8772a0866da87d5be6a267225
RyanMirallia <ryanmirallia@gmail.com>
2026-01-24 18:01:22 -0300

    ingest.py update

--- ingest.py
+++ ingest.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import os
+from sqlalchemy import create_engine
 
 df = pd.read_csv('data/netflix_titles.csv')
 
-INPUT_PATH = 'data/netflix_titles.csv'
-OUTPUT_PATH = 'data/netflix_titles_cleaned.csv'
+INPUT_PATH = os.path.join('data', 'netflix_titles.csv')
+OUTPUT_PATH = os.path.join('data', 'netflix_titles_cleaned.csv')
+DB_PATH = 'sqlite:///' + os.path.join('data', 'netflix.db')
 
 def processa_dados():
 
     if not os.path.exists(INPUT_PATH):
-        print(f"arquivo {INPUT_PATH} não foi encontrado!")
+        print(f"file {INPUT_PATH} not found!")
         return
         
     df = pd.read_csv(INPUT_PATH)
@@ -27,8 +29,13 @@ def processa_dados():
 
     df['date_added'] = pd.to_datetime(df['date_added'].str.strip())
 
+    engine = create_engine(DB_PATH)
+
+    df.to_sql('titles', con=engine, if_exists='replace', index=False)
+    print(f"db updated path: {DB_PATH}")
+
     df.to_csv(OUTPUT_PATH, index=False)
-    print(f"arquivo limpo salvo em: {OUTPUT_PATH}")
+    print(f"file clear and saved: {OUTPUT_PATH}")
 
 if __name__ == "__main__":
     processa_dados()
